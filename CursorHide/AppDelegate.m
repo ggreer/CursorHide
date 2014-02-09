2f580438954dd3e551ea2daa79fe99ed80a1dc08
Geoff Greer <geoff@greer.fm>
2014-02-08 23:19:09 -0800

    Yay it works. Time to fix the UI.

--- CursorHide/AppDelegate.m
+++ CursorHide/AppDelegate.m
@@ -6,13 +6,55 @@
 //  Copyright (c) 2014 Geoff Greer. All rights reserved.
 //
 
+#include <ApplicationServices/ApplicationServices.h>
+#include <AppKit/NSStatusBar.h>
+
 #import "AppDelegate.h"
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+NSTimer *timer = nil;
+double timeout = 2.7;
+
+- (void)hideCursor {
+    void CGSSetConnectionProperty(int, int, CFStringRef, CFBooleanRef);
+    int _CGSDefaultConnection();
+    CFStringRef propertyString;
+
+    NSLog(@"Hiding cursor");
+    // Hack to make background cursor setting work
+    propertyString = CFStringCreateWithCString(NULL, "SetsCursorInBackground", kCFStringEncodingUTF8);
+    CGSSetConnectionProperty(_CGSDefaultConnection(), _CGSDefaultConnection(), propertyString, kCFBooleanTrue);
+    CFRelease(propertyString);
+    // Hide the cursor and wait
+    CGDisplayHideCursor(kCGDirectMainDisplay);
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    timer = [NSTimer scheduledTimerWithTimeInterval:timeout
+                                             target:self selector:@selector(hideCursor)
+                                           userInfo:nil repeats:NO];
+    [timer setTolerance:0.1]; // Save power
+
+    NSStatusBar *bar = [NSStatusBar systemStatusBar];
+    
+    NSStatusItem *theItem = [bar statusItemWithLength:NSVariableStatusItemLength];
+    
+    [theItem setTitle: NSLocalizedString(@"Tablet",@"")];
+    [theItem setHighlightMode:YES];
+//    [theItem setMenu:_menu];
+
+    [NSEvent addGlobalMonitorForEventsMatchingMask:NSMouseMovedMask handler:^(NSEvent *event) {
+        //setAcceptsMouseMovedEvents
+        if ([timer isValid]) {
+            [timer invalidate];
+        }
+        timer = [NSTimer scheduledTimerWithTimeInterval:timeout
+                            target:self selector:@selector(hideCursor)
+                            userInfo:nil repeats:NO];
+        [timer setTolerance:0.1]; // Save power
+        CGDisplayShowCursor(kCGDirectMainDisplay);
+    }];
 }
 
 @end
